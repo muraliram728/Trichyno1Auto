4d85be1d47a51caeb8ea416dde259dbb11ea70ac
muraliram728 <muraliraman728@gmail.com>
2025-02-11 16:44:13 +0530

    Removed .env from tracking

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBroiimZtg68L3n76qa5wCVqzQfC_1-zm4",
-  authDomain: "trichy-no-1-auto.firebaseapp.com",
-  projectId: "trichy-no-1-auto",
-  storageBucket: "trichy-no-1-auto.firebasestorage.app",
-  messagingSenderId: "407767508420",
-  appId: "1:407767508420:web:caae14037722087592f091",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
