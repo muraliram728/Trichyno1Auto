ba3f5770ad81bb70e2d822e7c7b7fee8af9a23b5
muraliram728 <muraliraman728@gmail.com>
2025-03-03 11:40:47 +0530

    change the minimum price KM

--- src/Components/Services/services.js
+++ src/Components/Services/services.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import './Services.css'; // Import the external CSS
+import React from "react";
+import "./Services.css"; // Import the external CSS
 
 const Services = () => {
-  const phoneNumbers = ['+919080076738', '+917603994107', '+918903123944']; // Your numbers
+  const phoneNumbers = ["+917603994107", "+918903123944"]; // Your numbers
 
   return (
     <div className="services-container">
@@ -10,7 +10,9 @@ const Services = () => {
       <ul className="services-list">
         {phoneNumbers.map((number, index) => (
           <li key={index}>
-            <a href={`tel:${number}`} className="phone-button">{number}</a>
+            <a href={`tel:${number}`} className="phone-button">
+              {number}
+            </a>
           </li>
         ))}
       </ul>
