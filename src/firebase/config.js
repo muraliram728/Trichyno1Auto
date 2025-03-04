57656193dc2a43064b8f12050b2dfd78f113016f
muraliram728 <muraliraman728@gmail.com>
2025-03-04 12:33:19 +0530

    added firebase hosting

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -10,7 +10,6 @@ const firebaseConfig = {
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
-console.log("Firebase API Key:", process.env.REACT_APP_FIREBASE_API_KEY);
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
